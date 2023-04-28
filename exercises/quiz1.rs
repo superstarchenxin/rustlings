bf3e4b9e6f6a5bbcfdf5b00ef5d971cd210535d5
chenxin <597839372@qq.com>
2023-04-28 22:36:31 +0000

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,12 +10,7 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
-// Put your function here!
-// fn calculate_price_of_apples {
-
-// Don't modify this function!
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
@@ -27,4 +22,13 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+// Put your function here!
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if num > 40 {
+        num
+    } else {
+        num * 2
+    }
 }
